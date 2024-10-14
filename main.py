bea10bceefe588fbb4b35c7ea44a454ea37d6706
Natalia Janik <nataliia.janik@gmail.com>
2024-10-14 19:55:20 +0200

    number of words in sentences function implementation

--- main.py
+++ main.py
@@ -1,9 +1,41 @@
-#Przykład otrzymania wartości wprowadzonej przy użyciu funkcji input().
-wyraz=input()
+import string
+from collections import defaultdict
 
-#W celu poprawnego działania kodu w ramach GitHub Classroom warto dodatkowo użyć funkcję strip()
-#To pozwoli na usunięcie spacji oraz innych "spacjopodobnych" znaków (tabulacja \t', przejście do nowej linii '\n' lub '\r' etc.) z "głowy" i "ogona" (lewej i prawej części wyrazu).
-wyraz=wyraz.strip()
+def searchForWordsInSentences():
+    results = []
+    n = int(input("Podaj liczbę zdań: "))
+    
+    word_to_docs = defaultdict(list)
+    
+    for i in range(n):
+        sentence = input(f"Podaj zdanie nr {i + 1}: ")
+        words = sentence.split()
+        word_count = defaultdict(int)
 
-#Wydruk na ekranie (w konsoli)
-print ('Ten wyraz został wprowadzony:', wyraz)
+        for word in words:
+            clean = clean_word(word)
+            if clean:
+                word_count[clean] += 1
+
+        for word, count in word_count.items():
+            word_to_docs[word].append((i, count))
+
+    m = int(input("Podaj liczbę szukanych słów: "))
+    
+    for j in range(m):
+        query = input(f"Podaj słowo nr {j + 1}: ")
+        clean_query = clean_word(query)
+
+        if clean_query in word_to_docs:
+            result = sorted(word_to_docs[clean_query], key=lambda x: (-x[1], x[0]))
+            results.append([doc[0] for doc in result])
+        else:
+            results.append([])
+    
+    for result in results:
+        print(result)
+
+def clean_word(word):
+    return word.strip(string.punctuation).lower()
+
+searchForWordsInSentences()
